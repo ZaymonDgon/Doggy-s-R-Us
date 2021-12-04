f754aa69054062b07f24f80a8f7eb42dc501ceea
unknown <jessedraper2@gmail.com>
2021-12-04 13:29:15 -0700

    more work on seed.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,16 +1,43 @@
-const sequelize = require('../config/connection');
-const {User} = require('../models/User');
-const {Appointments} = require('../models/Appointments');
-const {Company} = require('../models/Company');
-const { Employee } = require('../models/Employee');
-const { Packages } = require('../models/Packages');
-const { Pet } = require('../models/Pet');
-const { Role } = require('../models/Role');
+const sequelize = require("../config/connection");
+const { User, Appointments, Company, Employee, Packages, Pet, Role } = require("../models/");
 
-const userData = require('./userData.json');
-const appointmentsData = require('./appointmentsData.json');
-const companyData = require('./companyData.json');
-const employeeData = require('./employeeData.json');
-const packagesData = require('./packagesData.json');
-const petData = require('./petData.json');
-const roleData = require('./roleData.json');
+const userData = require("./userData.json");
+const appointmentsData = require("./appointmentsData.json");
+const companyData = require("./companyData.json");
+const employeeData = require("./employeeData.json");
+const packagesData = require("./packagesData.json");
+const petData = require("./petData.json");
+const roleData = require("./roleData.json");
+
+const seedUser = async () => {
+  await User.bulkCreate(userData);
+};
+const seedAppointments = async () => {
+  await Appointments.bulkCreate(appointmentsData);
+};
+const seedCompany = async () => {
+  await Company.bulkCreate(companyData);
+};
+const seedEmployee = async () => {
+ await Employee.bulkCreate(employeeData);
+};
+const seedPackages = async () => {
+ await Packages.bulkCreate(packagesData);
+};
+const seedPet = async () => {
+ await Pet.bulkCreate(petData);
+};
+const seedRole =async  () =>{
+ await   Role.bulkCreate(roleData);
+}
+const seedDatabase = () => {
+ seedUser();
+appointmentsData();
+companyData();
+employeeData();
+packagesData();
+petData();
+roleData();
+process.exit();
+}
+seedDatabase();
