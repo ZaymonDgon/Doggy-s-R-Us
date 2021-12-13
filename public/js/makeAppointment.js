32e73141afe9a549789b75a5a9ec9e8b7b43e95b
unknown <jessedraper2@gmail.com>
2021-12-13 00:03:29 -0700

    start building more handlebars

---
+++ public/js/makeAppointment.js
@@ -0,0 +1,3 @@
+//This file needs to handle the process of making an appointment with the company. if else loop to get through hardcoded data plus value capture from time and date.
+const weekday = ["Monday", "Tuesday", "Wednesday","Thursday", "Friday"]
+const timeslots
